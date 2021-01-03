20139bc06045a084017976729e5cbb29163976a7
Mike Tzou <Chocobo1@users.noreply.github.com>
2021-01-03 13:20:21 +0800

    Merge pull request #14074 from Chocobo1/dialog_size
    Remember dialog sizes

--- src/gui/optionsdialog.h
+++ src/gui/optionsdialog.h
@@ -31,6 +31,7 @@
 #include <QDialog>
 
 #include "base/net/downloadmanager.h"
+#include "base/settingvalue.h"
 
 class QAbstractButton;
 class QCloseEvent;
@@ -60,6 +61,8 @@ namespace Ui
 class OptionsDialog final : public QDialog
 {
     Q_OBJECT
+    Q_DISABLE_COPY(OptionsDialog)
+
     using ThisType = OptionsDialog;
 
     enum Tabs
@@ -97,9 +100,7 @@ private slots:
     void enableApplyButton();
     void toggleComboRatioLimitAct();
     void changePage(QListWidgetItem *, QListWidgetItem *);
-    void loadWindowState();
     void loadSplitterState();
-    void saveWindowState() const;
     void handleScanFolderViewSelectionChanged();
     void on_IpFilterRefreshBtn_clicked();
     void handleIPFilterParsed(bool error, int ruleCount);
@@ -165,7 +166,6 @@ private:
     // IP Filter
     bool isIPFilteringEnabled() const;
     QString getFilter() const;
-    bool m_refreshingIpFilter;
     // Queueing system
     bool isQueueingSystemEnabled() const;
     int getMaxActiveDownloads() const;
@@ -181,8 +181,15 @@ private:
     bool schedTimesOk();
 
     Ui::OptionsDialog *m_ui;
+    SettingValue<QSize> m_storeDialogSize;
+    SettingValue<QStringList> m_storeHSplitterSize;
+
     QAbstractButton *m_applyButton;
+
     AdvancedSettings *m_advancedSettings;
+
     QList<QString> m_addedScanDirs;
     QList<QString> m_removedScanDirs;
+
+    bool m_refreshingIpFilter = false;
 };
